a42744f0cf7eded8412f7d67cc877cd383cb6a42
zouzou <rezzouk-zoulikha@hotmail.fr>
2016-04-14 02:47:10 +0200

    version 2 du site cv

--- js/cv.js
+++ js/cv.js
@@ -1,58 +1,20 @@
-var lienProfil = document.getElementById('lien-profil'),
-    lienCompetences = document.getElementById('lien_competences'),
-    lienFormations = document.getElementById('lien_formations'),
-    lienExperience = document.getElementById('lien_experience'),
-    lienContact = document.getElementById('lien_contact'),
-    divProfil = document.getElementById('profil'),
-    divCompetences = document.getElementById('competences'),
-    divFormations = document.getElementById('formations'),
-    divExperience = document.getElementById('experience'),
-    divContact = document.getElementById('contact');
+var boutonEmail = document.getElementById('boutonEmail'),
+    iconeMenu = document.getElementById('iconeMenu'),
+    contentMenu = document.getElementById('contentMenu'),
+    iconeClose = document.getElementById('iconeClose');
 
-/****** profil******/
-lienProfil.addEventListener("click", function () {
-    divProfil.style.display = "block";
-    divCompetences.style.display = "none";
-    divFormations.style.display = "none";
-    divExperience.style.display = "none";
-    divContact.style.display = "none";
+/*fonction ouverture menu*/
+iconeMenu.addEventListener("click", function menu() {
+    contentMenu.style.display = "block";
+    iconeMenu.style.display = "none";
 });
-/****** fin profil******/
-/****** compétences******/
-lienCompetences.addEventListener("click", function () {
-    divCompetences.style.display = "block";
-    divProfil.style.display = "none";
-    divFormations.style.display = "none";
-    divExperience.style.display = "none";
-    divContact.style.display = "none";
-});
-/****** fin compétences******/
-/****** formations ******/
-lienFormations.addEventListener("click", function () {
-    divFormations.style.display = "block";
-    divProfil.style.display = "none";
-    divCompetences.style.display = "none";
-    divExperience.style.display = "none";
-    divContact.style.display = "none";
 
+iconeClose.addEventListener("click", function menu() {
+    contentMenu.style.display = "none";
+    iconeMenu.style.display = "block";
 });
-/****** fin formaions******/
-/****** experience ******/
-lienExperience.addEventListener("click", function () {
-    divExperience.style.display = "block";
-    divProfil.style.display = "none";
-    divCompetences.style.display = "none";
-    divFormations.style.display = "none";
-    divContact.style.display = "none";
-
+/*fonction mail*/
+boutonEmail.addEventListener("click", function mail() {
+    boutonEmail.innerHTML =
+        '<i class="fa fa-envelope" aria-hidden="true"></i>' + ' rezzouk.zoulikha@gmail.com';
 });
-/****** fin experience ******/
-/****** contact ******/
-lienContact.addEventListener("click", function () {
-    divContact.style.display = "block";
-    divProfil.style.display = "none";
-    divCompetences.style.display = "none";
-    divFormations.style.display = "none";
-    divExperience.style.display = "none";
-});
-/****** fin contact ******/
