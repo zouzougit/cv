19aca0e75709eba6d79a64ddb8a792bfadce5039
zouzou <rezzouk-zoulikha@hotmail.fr>
2016-04-24 21:29:29 +0200

    ajout de l'animation back to top

--- js/cv.js
+++ js/cv.js
@@ -18,3 +18,22 @@ boutonEmail.addEventListener("click", function mail() {
     boutonEmail.innerHTML =
         '<i class="fa fa-envelope" aria-hidden="true"></i>' + ' rezzouk.zoulikha@gmail.com';
 });
+/*scroll back-to-top*/
+$(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+        var the_id = $(this).attr("href");
+        /* le sélecteur $(html, body) permet de corriger un bug sur chrome 
+               et safari (webkit) */
+        $('html, body').animate({
+            scrollTop: $(the_id).offset().top
+        }, 'slow');
+        return false;
+    });
+});
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        $('a.back-to-top').fadeIn('slow');
+    } else {
+        $('a.back-to-top').fadeOut('slow');
+    }
+});
